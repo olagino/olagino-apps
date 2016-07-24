56ebfc3351718bd408f99b0e5fb98b75fa5bae95
olagino <olaginos-buero@outlook.de>
2016-07-24 16:19:03 +0200

    Edit script, remove ftrobopy-lines

--- script.py
+++ script.py
@@ -91,68 +91,26 @@ class FtcGuiApplication(TxtApplication):
         except:
             self.txt = None                                  # set TXT to "None" of connection failed
 
-        if not self.txt:
-            # display error of TXT could no be connected
-            err_msg = QLabel("Error connecting IO server")   # create the error message label
-            err_msg.setWordWrap(True)                        # allow it to wrap over several lines
-            err_msg.setAlignment(Qt.AlignCenter)             # center it horizontally
-            w.setCentralWidget(err_msg)                      # attach it to the main output area
-        else:
-            # initialization went fine. So the main gui
-            # is being drawn
-            button = QPushButton("Switch Ausgang 1")         # create a button labeled "Toggle O1"
-            button.clicked.connect(self.on_button_clicked)   # connect button to event handler
-            w.setCentralWidget(button)                       # attach it to the main output area
+        vbox = QVBoxLayout()
 
-            self.cw = CamWidget()
+        self.cw = CamWidget()
 
-            vbox = QVBoxLayout()
-            vbox.setSpacing(0)
-            vbox.setContentsMargins(0,0,0,0)
+        vbox.addWidget(self.cw)
 
-            vbox.addWidget(self.cw)
-            vbox.addStretch()
-
-            self.lbl = QLabel()
-            self.lbl.setObjectName("smalllabel")
-            self.lbl.setAlignment(Qt.AlignCenter)
-            self.lbl.setWordWrap(True)
-            vbox.addWidget(self.lbl)
+        self.lbl = QLabel()
+        self.lbl.setObjectName("smalllabel")
+        self.lbl.setAlignment(Qt.AlignCenter)
+        self.lbl.setWordWrap(True)
+        vbox.addWidget(self.lbl)
 
 
-            self.connect( self.cw, SIGNAL("code(QString)"),
-                          self.on_code_detected )
-
-
-
-        # configure all TXT outputs to normal mode
-            M = [ self.txt.C_OUTPUT, self.txt.C_OUTPUT, self.txt.C_OUTPUT, self.txt.C_OUTPUT ]
-            I = [ (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ),
-                  (self.txt.C_SWITCH, self.txt.C_DIGITAL ) ]
-            self.txt.setConfig(M, I)
-            self.txt.updateConfig()
-
-        # initially switch light on
-            self.light_on = True                             # remember that the light is on
-            self.txt.setPwm(0,0)                           # set PWm to 512 (full on)
+        self.connect( self.cw, SIGNAL("code(QString)"),
+                      self.on_code_detected )
 
         w.show()
         self.exec_()
 
-    # an event handler for our button (called a "slot" in qt)
-    # it will be called whenever the user clicks the button
-    def on_button_clicked(self):
-        self.light_on = not self.light_on                   # change state
-        if self.light_on:                                   # set output accordingly
-            self.txt.setPwm(0,512)                          # PWN=512 means full on
-        else:
-            self.txt.setPwm(0,0)                            # PWM=0 means off
+
     def on_code_detected(self,str):
         self.lbl.setText(str)
 #        self.hide_timer.start(1000)  # hide after 1 second
